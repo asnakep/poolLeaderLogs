8dd3ec2d98dbbcba5fe0e9e4a07d8196a59abaaa
asnakep <econcotelli@gmail.com>
2021-11-04 14:29:46 +0000

    last steps

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # ScheduledBlocks
 Scheduled Block Checker for Cardano Stakepool Operators
 
-Lightweight and Portable Scheduled Blocks Checker for Current Epoch.
-No cardano-node Required, data is taken from blockfrost.io
+Lightweight and Portable Scheduled Blocks Checker for Next, Current and Previous Epochs.
+No cardano-node Required, data is taken from blockfrost.io and Armada Alliance
 
 Note: This is a reworking of old python script leaderLogs.py 
 available on https://github.com/papacarp/pooltool.io.git
-            
+
 
 ## Prerequisites:
 - Python 3.8
@@ -14,21 +14,26 @@ available on https://github.com/papacarp/pooltool.io.git
 - libsodium library
 
 ## Setup:
-- clone this repository using git: ``` git clone https://github.com/adasnakepool/ScheduledBlocks.git ```
+- clone this repository using git: ``` git clone https://github.com/asnakep/ScheduledBlocks.git ```
 - execute inside the newly cloned directory: ```pip install -r requirements.txt   ```  to install all needed python package requirements
 - get a project id on blockfrost.io
 - make sure you can access your vrf.skey file (you can copy in it a path of your choice) and remember to keep it as read only ``` chmod 400 vrf.skey ```
-- copy the config.yaml.default into a new file and call it **config.yaml** (do not remove the default if you want to update the script via git pull in future)
-- open the newly created config.yaml and configure the settings accordingly:
-  - **timezone**: your timezone (default: "Europe/Berlin")
-  - **blockfrost_id**: project id from blockfrost
-  - **pool_id**: your pool id. You can get it from adapools (it's the last part of the url)
-  - **pool_ticker**: your pool ticker
-  - **vrf_key**: "the path (relative or absolute) to the vrf.skey file
+
+- Set Variables on lines 23, 27-30 of ScheduledBlocks.py:
+
+### Set your own timezone -----------------------------------------###
+local_tz = pytz.timezone('')
+
+### Set These Variables ###
+BlockFrostId = ""
+PoolId = ""
+PoolTicker = ""
+VrfKeyFile = ('')
+### -------------------------------------------------------------- ###
+
 
 ## Usage:
 ``` python3 ScheduledBlocks.py ```
 
 ## Output: 
-- a *console output* with all the slots assigned for the latest available epoch
-- a SQLite database called: *slotschedule.db* which contains the slots assigned for every epoch the tool has been run.
+- a *console output* with all the slots assigned for next, current and previous Epochs
--- ScheduledBlocks.py
+++ ScheduledBlocks.py
@@ -1,239 +1,290 @@
 #!/bin/env python3
+
 import requests
+import urllib.request
 import math
 import binascii
 import json
 import pytz
-from sqlalchemy.sql.sqltypes import BigInteger
-import yaml
 import hashlib
+import re
+import readchar
 from ctypes import *
-from decimal import *
 from os import path
-from datetime import datetime
+from datetime import datetime, timezone
 from sys import exit, platform
-from sqlalchemy import Column
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.engine.create import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 
-#################################
-######### configuration #########
-#################################
-BASE_DIR = path.dirname(path.abspath(__file__))
-CONFIGPATH = path.join(BASE_DIR, "config.yaml")
-with open(CONFIGPATH) as f:
-    CONFIG = yaml.load(f, Loader=yaml.FullLoader)
-
-##################################
-######### setup slots db #########
-##################################
-Base = declarative_base()
-engine = create_engine("sqlite:///slotschedule.db")
-session = scoped_session(sessionmaker(autocommit=False,bind=engine))
-
-class Slots(Base):
-    __tablename__ = "slots"
-    epoch = Column(BigInteger, primary_key=True)
-    slot_qty = Column(BigInteger)
-    slots = Column(BigInteger)
-
-Base.metadata.create_all(engine)
-
-### --------------------- Settings ------------------------------- ###
-local_tz = pytz.timezone(CONFIG["timezone"])
-BlockFrostId = CONFIG["blockfrost_id"]
-PoolId = CONFIG["pool_id"]
-PoolTicker = CONFIG["pool_ticker"]
-VrfKeyFile = CONFIG["vrf_key"]
-### -------------------------------------------------------------- ###
-### ADA Unicode symbol and Lovelaces removal ###
-ada = " \u20B3"
-lovelaces = 1000000
-
-
-##################################
-######### main routines ##########
-##################################
 class col:
+    red = '\033[31m'
     green = '\033[92m'
     endcl = '\033[0m'
     bold = '\033[1m'
 
+### Set your own timezone -----------------------------------------###
+local_tz = pytz.timezone('Europe/Berlin')
 
-class SlotLeaderCheck:
-    ### Get data from blockfrost.io APIs ###
-    headers = {'content-type': 'application/json', 'project_id': BlockFrostId}
+### Set These Variables ###
+BlockFrostId = "jsLX87IunMrsi3BsHenNlKHecq4CxlhJ"
+PoolId = "342350284fd76ba9dbd7fd4ed579b2a2058d5ee558f8872b37817b28"
+PoolTicker = "SNAKE"
+VrfKeyFile = ('/home/nodo/.SlotLeader/vrf/vrf.skey')
+### -------------------------------------------------------------- ###
 
-    epochParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/latest/parameters", headers=headers)
-    json_data = epochParam.json()
-    epoch = epochParam.json().get("epoch") 
-    eta0 = epochParam.json().get("nonce")
 
-    poolSigma = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId, headers=headers)
-    json_data = poolSigma.json()
-    sigma = poolSigma.json().get("active_size")
+### ADA Unicode symbol and Lovelaces removal ###
+ada = " \u20B3"
+lovelaces = 1000000
 
-    netStakeParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/latest", headers=headers)
-    json_data = netStakeParam.json()
-    nStake = int(netStakeParam.json().get("active_stake")) / lovelaces
-    nStake = "{:,}".format(nStake)
+### Get Current Epoch from Armada Alliance ###
+headers_armada ={'content-type': 'application/json'}
+CepochParam = requests.get("https://nonce.armada-alliance.com/current", headers=headers_armada)
+json_data = CepochParam.json()
+Cepoch = CepochParam.json().get("epoch")
 
-    poolStakeParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId, headers=headers)
-    json_data = poolStakeParam.json()
-    pStake = int(poolStakeParam.json().get("active_stake")) / lovelaces
-    pStake = "{:,}".format(pStake)
+NepochParam = requests.get("https://nonce.armada-alliance.com/next", headers=headers_armada)
+json_data = NepochParam.json()
+Nepoch = NepochParam.json().get("epoch")
+Neta0 = NepochParam.json().get("nonce")
 
-    ### Slots Leader Computation ###
 
-    ### Opening vrf.skey file ###
-    with open(VrfKeyFile) as f:
-            skey = json.load(f)
-            poolVrfSkey = skey['cborHex'][4:]
+ErrorMsg = "Query returned no rows"
+if ErrorMsg in Neta0 :
+ msg = str(col.red + f'(New Nonce Not Avaliable Yet)')
 
-    ### Determine libsodium path based on platform ###
-    Libsodium = None
-    if platform == "linux" or platform == "linux2":
-        # Bindings are not avaliable so using ctypes to just force it in for now.
-        Libsodium = cdll.LoadLibrary("/usr/local/lib/libsodium.so")
-    elif platform == "darwin":
-        # Try both Daedalus' bundled libsodium and a system-wide libsodium path.
-        daedalusLibsodiumPath = path.join("/Applications", "Daedalus Mainnet.app", "Contents", "MacOS", "libsodium.23.dylib")
-        systemLibsodiumPath = path.join("/usr", "local", "lib", "libsodium.23.dylib")
+if ErrorMsg not in Neta0 :
+ msg = str(col.green + f'(Next Epoch Nonce Available)')
 
-        if path.exists(daedalusLibsodiumPath):
-            Libsodium = cdll.LoadLibrary(daedalusLibsodiumPath)
-        elif path.exists(systemLibsodiumPath):
-            Libsodium = cdll.LoadLibrary(systemLibsodiumPath)
-        else:
-            exit(f'Unable to find libsodium, checked the following paths: {", ".join([daedalusLibsodiumPath, systemLibsodiumPath])}')
-    
-    Libsodium.sodium_init()
-    ################################################## ###
+### User Prompt for specific prev/curr Epochs
+print()
+print(col.bold + col.green + f'Welcome to ScheduledBlocks for Cardano SPOs. ')
+print()
+print(col.green + f'Check Assigned Blocks in Next, Current and Previous Cardano Epochs.')
+print(col.endcl)
+print(col.bold + col.green + f'Current Epoch: ' + col.endcl + col.bold +str(Cepoch))
+print(col.endcl)
+print()
+print(col.bold + f'(N) to check Next Epoch Schedules ' +str(msg))
+print(col.endcl)
+print(col.bold + f'(E) to Check in Current or Previous Epochs')
+print()
+print(f'(X) to Exit')
+print(col.endcl)
 
-    ### Get Blockchain Genesis Parameters from blockfrost.io ###
-    GenesisParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/genesis", headers=headers)
-    json_data = GenesisParam.json()
+### Read Keyboard keys ###
+key = readchar.readkey()
 
-    epochLength = GenesisParam.json().get("epoch_length")
-    activeSlotCoeff = GenesisParam.json().get("active_slots_coefficient")
-    slotLength = GenesisParam.json().get("slot_length")
+if(key == 'X'):
+ exit()
 
-    ### Epoch211FirstSlot ###
-    firstShelleySlot = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/blocks/4555184", headers=headers)
-    json_data = firstShelleySlot.json()
-    firstSlot = firstShelleySlot.json().get("slot")
+if(key == 'N'):
 
-    ### calculate first slot of target epoch ###
-    firstSlotOfEpoch = (firstSlot) + (epoch - 211)*epochLength
+### Get data from Armada Alliance and Blockfrost.io ###
 
-    getcontext().prec = 9
-    getcontext().rounding = ROUND_HALF_UP
+ headers = {'content-type': 'application/json', 'project_id': BlockFrostId}
+ headers_armada ={'content-type': 'application/json'}
 
-    # mkseed
-    def mkSeed(self,slot,eta0):
+ epochParam = requests.get("https://nonce.armada-alliance.com/next", headers=headers_armada)
+ json_data = epochParam.json()
+ epoch = epochParam.json().get("epoch")
+ eta0 = epochParam.json().get("nonce")
 
-        h = hashlib.blake2b(digest_size=32)
-        h.update(bytearray([0,0,0,0,0,0,0,1])) #neutral nonce
-        seedLbytes=h.digest()
+ ErrorMsg = "Query returned no rows"
+ if ErrorMsg in eta0 :
+  print(col.bold + col.red + f'New Nonce Not Avaliable Yet for Epoch: '+ col.endcl + col.bold + str(epoch))
+  print(col.endcl)
+  exit()
 
-        h = hashlib.blake2b(digest_size=32)
-        h.update(slot.to_bytes(8,byteorder='big') + binascii.unhexlify(eta0))
-        slotToSeedBytes = h.digest()
+ if ErrorMsg not in eta0 :
+  print(f'New Epoch Nonce: '+str(eta0))
 
-        seed = [x ^ slotToSeedBytes[i] for i,x in enumerate(seedLbytes)]
-        return bytes(seed)
+ netStakeParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/"+(str(epoch-1)), headers=headers)
+ json_data = netStakeParam.json()
+ nStake = int(netStakeParam.json().get("active_stake")) / lovelaces
+ nStake = "{:,}".format(nStake)
 
-    # eval certified
-    def vrfEvalCertified(self, seed, tpraosCanBeLeaderSignKeyVRF):
-        if isinstance(seed, bytes) and isinstance(tpraosCanBeLeaderSignKeyVRF, bytes):
-            proof = create_string_buffer(self.Libsodium.crypto_vrf_ietfdraft03_proofbytes())
+ poolStakeParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId, headers=headers)
+ json_data = poolStakeParam.json()
+ pStake = int(poolStakeParam.json().get("active_stake")) / lovelaces
+ pStake = "{:,}".format(pStake)
 
-            self.Libsodium.crypto_vrf_prove(proof, tpraosCanBeLeaderSignKeyVRF,seed, len(seed))
+ poolSigma = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId, headers=headers)
+ json_data = poolSigma.json()
+ sigma = poolSigma.json().get("active_size")
 
-            proofHash = create_string_buffer(self.Libsodium.crypto_vrf_outputbytes())
+ print()
+ print(col.bold + f'Checking SlotLeader Schedules for Stakepool: ' + (col.green + PoolTicker + col.endcl))
+ print()
+ print(col.bold + f'Pool Id: ' + (col.green + PoolId + col.endcl))
+ print()
+ print(col.bold + f'Next Epoch: ' + col.green + str(epoch) + col.endcl)
+ print()
+ print(col.bold + f'New Nonce: ' + col.green + str(eta0) + col.endcl)
+ print()
+ print(col.bold + f'Network Active Stake in Epoch ' + str(epoch-1) + ": " + col.green + str(nStake) + col.endcl + col.bold + ada + col.endcl)
+ print()
+ print(col.bold + f'Pool Active Stake in Epoch ' + str(epoch-1) + ": " + col.green + str(pStake) + col.endcl + col.bold + ada + col.endcl)
+ print()
 
-            self.Libsodium.crypto_vrf_proof_to_hash(proofHash,proof)
 
-            return proofHash.raw
+if(key == 'E'):
 
-        else:
-            print("error.  Feed me bytes")
-            exit()
+ print()
+ Epoch = input(col.bold + "Enter Epoch Number (Previous or Current): " + col.green)
+ print(col.endcl)
 
-    # Determine if our pool is a slot leader for this given slot
-    # @param slot The slot to check
-    # @param activeSlotCoeff The activeSlotsCoeff value from protocol params
-    # @param sigma The controlled stake proportion for the pool
-    # @param eta0 The epoch nonce value
-    # @param poolVrfSkey The vrf signing key for the pool
+### Get data from blockfrost.io APIs ###
 
-    def isSlotLeader(self, slot,activeSlotCoeff,sigma,eta0,poolVrfSkey):
-        seed = self.mkSeed(slot, eta0)
-        tpraosCanBeLeaderSignKeyVRFb = binascii.unhexlify(poolVrfSkey)
-        cert=self.vrfEvalCertified(seed,tpraosCanBeLeaderSignKeyVRFb)
-        certNat  = int.from_bytes(cert, byteorder="big", signed=False)
-        certNatMax = math.pow(2,512)
-        denominator = certNatMax - certNat
-        q = certNatMax / denominator
-        c = math.log(1.0 - activeSlotCoeff)
-        sigmaOfF = math.exp(-sigma * c)
-        return q <= sigmaOfF
+ headers = {'content-type': 'application/json', 'project_id': BlockFrostId}
 
-    def checkLeaderSlots(self,verbose):
+ epochParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/"+Epoch+"/parameters", headers=headers)
+ json_data = epochParam.json()
+ epoch = epochParam.json().get("epoch")
+ eta0 = epochParam.json().get("nonce")
 
-        if verbose == True:
-            print()
-            print(col.bold + f'Checking SlotLeader Schedules for Stakepool: ' + (col.green + PoolTicker + col.endcl))
-            print()
-            print(col.bold + f'Pool Id: ' + (col.green + PoolId + col.endcl))
-            print()
-            print(col.bold + f'Current Epoch: ' + col.green + str(self.epoch) + col.endcl)
-            print()
-            print(col.bold + f'Nonce: ' + col.green + str(self.eta0) + col.endcl)
-            print()
-            print(col.bold + f'Network Active Stake: ' + col.green + str(self.nStake) + col.endcl + col.bold + ada + col.endcl)
-            print()
-            print(col.bold + f'Pool Active Stake: ' + col.green + str(self.pStake) + col.endcl + col.bold + ada + col.endcl)
-            print()
+ netStakeParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/"+Epoch, headers=headers)
+ json_data = netStakeParam.json()
+ nStake = int(netStakeParam.json().get("active_stake")) / lovelaces
+ nStake = "{:,}".format(nStake)
 
-        slotcount=0
-        slots = []
+ poolHistStake = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId+"/history", headers=headers)
+ json_data = poolHistStake.json()
 
-        for slot in range(self.firstSlotOfEpoch,self.epochLength+self.firstSlotOfEpoch):
+ for i in json_data :
+  if i['epoch'] == int(Epoch) :
+   sigma = (i["active_size"])
 
-            slotLeader = self.isSlotLeader(slot, self.activeSlotCoeff, self.sigma, self.eta0, self.poolVrfSkey)
+ for i in json_data :
+  if i['epoch'] == int(Epoch) :
+   pStake = (i["active_stake"])
+   pStake = int(pStake) / lovelaces
+   pStake = "{:,}".format(pStake)
 
-            if slotLeader:
-                timestamp = datetime.fromtimestamp(slot + 1591566291, tz=local_tz)
-                slots.append(slot)
-                slotcount+=1
-                if verbose == True:
-                    print(col.bold + "Leader At Slot: "  + str(slot-self.firstSlotOfEpoch) + " - Local Time " + str(timestamp.strftime('%Y-%m-%d %H:%M:%S') + " - Scheduled Epoch Blocks: " + str(slotcount)))
 
-        if slotcount == 0:
-            if verbose == True:
-                print(col.bold + "No SlotLeader Schedules Found for Current Epoch " +str(self.epoch))
+ print(col.bold + f'Checking SlotLeader Schedules for Stakepool: ' + (col.green + PoolTicker + col.endcl))
+ print()
+ print(col.bold + f'Pool Id: ' + (col.green + PoolId + col.endcl))
+ print()
+ print(col.bold + f'Epoch: ' + col.green + Epoch + col.endcl)
+ print()
+ print(col.bold + f'Nonce: ' + col.green + str(eta0) + col.endcl)
+ print()
+ print(col.bold + f'Network Active Stake in Epoch ' + Epoch + ": " + col.green + str(nStake) + col.endcl + col.bold + ada + col.endcl)
+ print()
+ print(col.bold + f'Pool Active Stake in Epoch ' + Epoch + ": " + col.green + str(pStake) + col.endcl + col.bold + ada + col.endcl)
+ print()
 
-        # save to SQLite database
-        try: 
-            newSlots = Slots(
-                        epoch = self.epoch,
-                        slot_qty = slotcount,
-                        slots = str(slots)
-                    )
-            session.merge(newSlots)
-            session.commit()
-        except Exception as ex:
-            print(ex)
 
-        # default constructor
-    def __init__(self,verbose=True):
-        self.checkLeaderSlots(verbose)
+### Calculate Slots Leader ###
 
-####################################
-######### main shell exec ##########
-####################################
-if __name__ == "__main__":
-    # run the leaderslot check function
-    SlotLeaderCheck()
+### Opening vrf.skey file ###
+with open(VrfKeyFile) as f:
+        skey = json.load(f)
+        poolVrfSkey = skey['cborHex'][4:]
+
+### Determine libsodium path based on platform ###
+libsodium = None
+if platform == "linux" or platform == "linux2":
+    # Bindings are not avaliable so using ctypes to just force it in for now.
+    libsodium = cdll.LoadLibrary("/usr/local/lib/libsodium.so")
+elif platform == "darwin":
+    # Try both Daedalus' bundled libsodium and a system-wide libsodium path.
+    daedalusLibsodiumPath = path.join("/Applications", "Daedalus Mainnet.app", "Contents", "MacOS", "libsodium.23.dylib")
+    systemLibsodiumPath = path.join("/usr", "local", "lib", "libsodium.23.dylib")
+
+    if path.exists(daedalusLibsodiumPath):
+        libsodium = cdll.LoadLibrary(daedalusLibsodiumPath)
+    elif path.exists(systemLibsodiumPath):
+        libsodium = cdll.LoadLibrary(systemLibsodiumPath)
+    else:
+        exit(f'Unable to find libsodium, checked the following paths: {", ".join([daedalusLibsodiumPath, systemLibsodiumPath])}')
+libsodium.sodium_init()
+################################################## ###
+
+### Blockchain Genesis Parameters ###
+headers = {'content-type': 'application/json', 'project_id': BlockFrostId}
+GenesisParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/genesis", headers=headers)
+json_data = GenesisParam.json()
+
+epochLength = GenesisParam.json().get("epoch_length")
+activeSlotCoeff = GenesisParam.json().get("active_slots_coefficient")
+slotLength = GenesisParam.json().get("slot_length")
+
+### Epoch211FirstSlot ###
+firstShelleySlot = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/blocks/4555184", headers=headers)
+json_data = firstShelleySlot.json()
+firstSlot = firstShelleySlot.json().get("slot")
+
+### calculate first slot of target epoch ###
+firstSlotOfEpoch = (firstSlot) + (epoch - 211)*epochLength
+
+from decimal import *
+getcontext().prec = 9
+getcontext().rounding = ROUND_HALF_UP
+
+def mkSeed(slot,eta0):
+
+    h = hashlib.blake2b(digest_size=32)
+    h.update(bytearray([0,0,0,0,0,0,0,1])) #neutral nonce
+    seedLbytes=h.digest()
+
+    h = hashlib.blake2b(digest_size=32)
+    h.update(slot.to_bytes(8,byteorder='big') + binascii.unhexlify(eta0))
+    slotToSeedBytes = h.digest()
+
+    seed = [x ^ slotToSeedBytes[i] for i,x in enumerate(seedLbytes)]
+
+    return bytes(seed)
+
+def vrfEvalCertified(seed, tpraosCanBeLeaderSignKeyVRF):
+    if isinstance(seed, bytes) and isinstance(tpraosCanBeLeaderSignKeyVRF, bytes):
+        proof = create_string_buffer(libsodium.crypto_vrf_ietfdraft03_proofbytes())
+
+        libsodium.crypto_vrf_prove(proof, tpraosCanBeLeaderSignKeyVRF,seed, len(seed))
+
+        proofHash = create_string_buffer(libsodium.crypto_vrf_outputbytes())
+
+        libsodium.crypto_vrf_proof_to_hash(proofHash,proof)
+
+        return proofHash.raw
+
+    else:
+        print("error.  Feed me bytes")
+        exit()
+
+# Determine if our pool is a slot leader for this given slot
+# @param slot The slot to check
+# @param activeSlotCoeff The activeSlotsCoeff value from protocol params
+# @param sigma The controlled stake proportion for the pool
+# @param eta0 The epoch nonce value
+# @param poolVrfSkey The vrf signing key for the pool
+
+def isSlotLeader(slot,activeSlotCoeff,sigma,eta0,poolVrfSkey):
+    seed = mkSeed(slot, eta0)
+    tpraosCanBeLeaderSignKeyVRFb = binascii.unhexlify(poolVrfSkey)
+    cert=vrfEvalCertified(seed,tpraosCanBeLeaderSignKeyVRFb)
+    certNat  = int.from_bytes(cert, byteorder="big", signed=False)
+    certNatMax = math.pow(2,512)
+    denominator = certNatMax - certNat
+    q = certNatMax / denominator
+    c = math.log(1.0 - activeSlotCoeff)
+    sigmaOfF = math.exp(-sigma * c)
+    return q <= sigmaOfF
+
+
+slotcount=0
+
+for slot in range(firstSlotOfEpoch,epochLength+firstSlotOfEpoch):
+
+    slotLeader = isSlotLeader(slot, activeSlotCoeff, sigma, eta0, poolVrfSkey)
+
+    if slotLeader:
+        pass
+        timestamp = datetime.fromtimestamp(slot + 1591566291, tz=local_tz)
+        slotcount+=1
+
+        print(col.bold + "Leader At Slot: "  + str(slot-firstSlotOfEpoch) + " - Local Time " + str(timestamp.strftime('%Y-%m-%d %H:%M:%S') + " - Scheduled Epoch Blocks: " + str(slotcount)))
+
+if slotcount == 0:
+    print(col.bold + "No SlotLeader Schedules Found for Current Epoch " +str(epoch))
+    exit
+
