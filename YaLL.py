0aa3a4c006485dd046cecb3d18f5ddd8f8467736
SNAKE (Cardano Stakepool) <53517506+asnakep@users.noreply.github.com>
2022-10-08 11:10:18 +0200

    Update YaLL.py
    Deployed new version with new epochNonce Computation.

--- YaLL.py
+++ YaLL.py
@@ -9,6 +9,7 @@ import pytz
 import hashlib
 import re
 import readchar
+import subprocess as sp
 from ctypes import *
 from os import system, path
 from datetime import datetime, timezone
@@ -24,134 +25,249 @@ def ClearScreen():
     system(command)
 
 ### Set your own timezone -----------------------------------------###
-local_tz = pytz.timezone('')
+### Eg: local_tz = pytz.timezone('Europe/Berlin')                  ###
 
-### Eg: local_tz = pytz.timezone('Europe/Berlin')
+local_tz = pytz.timezone('')
 
 ### Set These Variables ###
 BlockFrostId = ""
-PoolId = ""
-PoolTicker = ""
-VrfKeyFile = (<your_vrf.skey_path>/vrf.skey)
+PoolId       = "Hash Format, not bech32"
+PoolTicker   = ""
+VrfKeyFile   = ('<your-path-to>/vrf.skey')
 ### -------------------------------------------------------------- ###
 
-
 ### ADA Unicode symbol and Lovelaces removal ###
-ada = " \u20B3"
+ada       = " \u20B3"
 lovelaces = 1000000
 
-### Get Current Epoch ###
-headers = {'content-type': 'application/json', 'project_id': BlockFrostId}
+### Get Current Epoch from BlockFrost ###
+headers    = {'content-type': 'application/json', 'project_id': BlockFrostId}
 epochParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/latest/parameters", headers=headers)
-json_data = epochParam.json()
-epoch = epochParam.json().get("epoch")
- 
+json_data  = epochParam.json()
+epoch      = epochParam.json().get("epoch")
 
 ### User Prompt ###
+ClearScreen()
 print()
-print(col.green + f'Welcome to ScheduledBlocks for Cardano SPOs. ')
+print(col.green + f'Welcome to YaLL (Yet Another Leader Logs) for Cardano SPOs. ')
 print()
-print(col.green + f'Check Assigned Blocks in Current Cardano Epochs.')
+print(col.green + f'Check Leader Logs in Next, Current and Previous Cardano Epochs.')
 print(col.endcl)
 print(col.green + f'Current Epoch: ' + col.endcl +str(epoch))
 print(col.endcl)
-print(f'(c) to Check Current Epoch')
+print(f'(n) to Check Next Epoch Leader Logs')
 print(col.endcl)
-print(f'(any key) to Exit')
+print(f'(c) to Check Current Epoch Leader Logs')
 print(col.endcl)
+print(f'(p) to Check Previous Epochs Leader Logs')
+print(col.endcl)
+print(f'(any other key) to Exit')
+
 
 ### Read Keyboard keys ###
 key = readchar.readkey()
 
+if(key == 'n'):
+
+### Extract newEpochNonce from cardano-node ###
+### Get Current Epoch and Current epoch_slot from BlockFrost ###
+
+  ClearScreen()
+
+  headers      = {'content-type': 'application/json', 'project_id': BlockFrostId}
+
+  epochParam   = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/latest", headers=headers)
+  json_data    = epochParam.json()
+  epoch        = epochParam.json().get("epoch")
+  epoch        = int(epoch + 1)
+
+  latestBlocks = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/blocks/latest", headers=headers)
+  json_data    = latestBlocks.json()
+  epochSlot    = latestBlocks.json().get("epoch_slot")
+
+
+  ### Check Next Epoch Leader Logs ###
+
+  if epochSlot >= 302400:
+
+    ### Take "candidateNonce" from protocol-state ###
+    candidateNonce      = sp.getoutput('cardano-cli query protocol-state --mainnet | jq -r .candidateNonce.contents')
+
+    ### Take "lastEpochBlockNonce" from protocol-state ###
+    lastEpochBlockNonce = sp.getoutput("cardano-cli query protocol-state --mainnet | jq -r .lastEpochBlockNonce.contents")
+
+    ### Extract newEpochNonce ###
+    print()
+    eta0 = hashlib.blake2b(bytes.fromhex(candidateNonce + lastEpochBlockNonce),digest_size=32).hexdigest()
+    print(f'New epochNonce: ' + col.green + str(eta0) + col.endcl)
+
+    ### Network and Pool Data from BlockFrost ###
+    netStakeParam  = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/latest", headers=headers)
+    json_data      = netStakeParam.json()
+    nStake         = int(netStakeParam.json().get("active_stake")) / lovelaces
+    nStake         = "{:,}".format(nStake)
+
+    poolStakeParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId, headers=headers)
+    json_data      = poolStakeParam.json()
+    pStake         = int(poolStakeParam.json().get("active_stake")) / lovelaces
+    pStake         = "{:,}".format(pStake)
+
+    poolSigma      = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId, headers=headers)
+    json_data      = poolSigma.json()
+    sigma          = poolSigma.json().get("active_size")
+
+    ### Message ###
+    print()
+    print(f'Checking Leader Logs for Stakepool: ' + (col.green + PoolTicker + col.endcl))
+    print()
+    print(f'Pool Id: '    + (col.green + PoolId + col.endcl))
+    print()
+    print(f'Next Epoch: ' + col.green + str(epoch) + col.endcl)
+    print()
+    print(f'Network Active Stake in Epoch ' + str(epoch-1) + ": " + col.green + str(nStake) + col.endcl + ada + col.endcl)
+    print()
+    print(f'Pool Active Stake in Epoch '    + str(epoch-1) + ": " + col.green + str(pStake) + col.endcl + ada + col.endcl)
+    print()
+
+
+  if epochSlot < 302400:
+    print()
+    print(f'New epochNonce Not Yet Computable for Epoch: ' + str(epoch))
+    print()
+    print(f'Come back at epochSlot 302400.')
+    print()
+    print(f'Current epochSlot '+ str(epochSlot) + '.')
+    print()
+    exit()
+
+
+### Check Current Epoch Leader Logs ###
 
 if(key == 'c'):
 
- ClearScreen()
+  ClearScreen()
 
- headers = {'content-type': 'application/json', 'project_id': BlockFrostId}
+  headers        = {'content-type': 'application/json', 'project_id': BlockFrostId}
 
- epochParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/latest/parameters", headers=headers)
- json_data = epochParam.json()
- epoch = epochParam.json().get("epoch")
- eta0 = epochParam.json().get("nonce")
+  epochParam     = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/latest/parameters", headers=headers)
+  json_data      = epochParam.json()
+  epoch          = epochParam.json().get("epoch")
+  eta0           = epochParam.json().get("nonce")
 
- netStakeParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/latest", headers=headers)
- json_data = netStakeParam.json()
- nStake = int(netStakeParam.json().get("active_stake")) / lovelaces
- nStake = "{:,}".format(nStake)
+  netStakeParam  = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/latest", headers=headers)
+  json_data      = netStakeParam.json()
+  nStake         = int(netStakeParam.json().get("active_stake")) / lovelaces
+  nStake         = "{:,}".format(nStake)
 
- poolStakeParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId, headers=headers)
- json_data = poolStakeParam.json()
- pStake = int(poolStakeParam.json().get("active_stake")) / lovelaces
- pStake = "{:,}".format(pStake)
+  poolStakeParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId, headers=headers)
+  json_data      = poolStakeParam.json()
+  pStake         = int(poolStakeParam.json().get("active_stake")) / lovelaces
+  pStake         = "{:,}".format(pStake)
 
- poolSigma = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId, headers=headers)
- json_data = poolSigma.json()
- sigma = poolSigma.json().get("active_size")
-
- print()
- print(f'Checking SlotLeader Schedules for Stakepool: ' + (col.green + PoolTicker + col.endcl))
- print()
- print(f'Pool Id: ' + (col.green + PoolId + col.endcl))
- print()
- print(f'Epoch: ' + col.green + str(epoch) + col.endcl)
- print()
- print(f'Nonce: ' + col.green + str(eta0) + col.endcl)
- print()
- print(f'Network Active Stake in Epoch ' + str(epoch) + ": " + col.green + str(nStake) + col.endcl + ada + col.endcl)
- print()
- print(f'Pool Active Stake in Epoch ' + str(epoch) + ": " + col.green + str(pStake) + col.endcl + ada + col.endcl)
- print()
+  poolSigma      = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId, headers=headers)
+  json_data      = poolSigma.json()
+  sigma          = poolSigma.json().get("active_size")
 
 
-### ############### ###
-if(key != 'c'):
- exit(0)
+  ### Message ###
+  print()
+  print(f'Checking Leader Logs for Stakepool: ' + (col.green + PoolTicker + col.endcl))
+  print()
+  print(f'Pool Id: ' + (col.green + PoolId + col.endcl))
+  print()
+  print(f'Epoch: '   + col.green + str(epoch) + col.endcl)
+  print()
+  print(f'Nonce: '   + col.green + str(eta0) + col.endcl)
+  print()
+  print(f'Network Active Stake in Epoch ' + str(epoch) + ": " + col.green + str(nStake) + col.endcl + ada + col.endcl)
+  print()
+  print(f'Pool Active Stake in Epoch '    + str(epoch) + ": " + col.green + str(pStake) + col.endcl + ada + col.endcl)
+  print()
 
 
-### Calculate Slots Leader ###
+### Check Previous Epochs Leader Logs ###
 
-### Opening vrf.skey file ###
+if(key == 'p'):
+
+  ClearScreen()
+  print()
+  Epoch = input("Enter Previous Epoch Number: " + col.green)
+  print(col.endcl)
+
+
+  ### Historical Network and Pool Data from BlockFrost ###
+
+  headers       = {'content-type': 'application/json', 'project_id': BlockFrostId}
+
+  epochParam    = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/"+Epoch+"/parameters", headers=headers)
+  json_data     = epochParam.json()
+  epoch         = epochParam.json().get("epoch")
+  eta0          = epochParam.json().get("nonce")
+
+  netStakeParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/epochs/"+Epoch, headers=headers)
+  json_data     = netStakeParam.json()
+  nStake        = int(netStakeParam.json().get("active_stake")) / lovelaces
+  nStake        = "{:,}".format(nStake)
+
+  poolHistStake = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/pools/"+PoolId+"/history?page=2", headers=headers)
+  json_data     = poolHistStake.json()
+
+  for i in json_data :
+      if i['epoch'] == int(Epoch) :
+          sigma  = (i["active_size"])
+          pStake = (i["active_stake"])
+          pStake = int(pStake) / lovelaces
+          pStake = "{:,}".format(pStake)
+
+  ### Message ###
+  print(f'Checking Leader Logs for Stakepool: ' + (col.green + PoolTicker + col.endcl))
+  print()
+  print(f'Pool Id: ' + (col.green + PoolId + col.endcl))
+  print()
+  print(f'Epoch: '   + col.green + Epoch + col.endcl)
+  print()
+  print(f'Nonce: '   + col.green + str(eta0) + col.endcl)
+  print()
+  print(f'Network Active Stake in Epoch ' + Epoch + ": " + col.green + str(nStake) + col.endcl + ada + col.endcl)
+  print()
+  print(f'Pool Active Stake in Epoch '    + Epoch + ": " + col.green + str(pStake) + col.endcl + ada + col.endcl)
+  print()
+
+
+### ######################################### ###
+if(key != 'n') and (key != 'c') and (key != 'p'):
+   exit(0)
+
+
+### Leader Logs Computation ###
+
+### Opening vrf.skey file ####
 with open(VrfKeyFile) as f:
         skey = json.load(f)
         poolVrfSkey = skey['cborHex'][4:]
 
-### Determine libsodium path based on platform ###
-libsodium = None
-if platform == "linux" or platform == "linux2":
-    # Bindings are not avaliable so using ctypes to just force it in for now.
-    libsodium = cdll.LoadLibrary("/usr/local/lib/libsodium.so")
-elif platform == "darwin":
-    # Try both Daedalus' bundled libsodium and a system-wide libsodium path.
-    daedalusLibsodiumPath = path.join("/Applications", "Daedalus Mainnet.app", "Contents", "MacOS", "libsodium.23.dylib")
-    systemLibsodiumPath = path.join("/usr", "local", "lib", "libsodium.so")
-
-    if path.exists(daedalusLibsodiumPath):
-        libsodium = cdll.LoadLibrary(daedalusLibsodiumPath)
-    elif path.exists(systemLibsodiumPath):
-        libsodium = cdll.LoadLibrary(systemLibsodiumPath)
-    else:
-        exit(f'Unable to find libsodium, checked the following paths: {", ".join([daedalusLibsodiumPath, systemLibsodiumPath])}')
+### Load libsodium library from /usr/local/lib/  ###
+libsodium = cdll.LoadLibrary("/usr/local/lib/libsodium.so")
 libsodium.sodium_init()
-################################################## ###
 
 ### Blockchain Genesis Parameters ###
-headers = {'content-type': 'application/json', 'project_id': BlockFrostId}
-GenesisParam = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/genesis", headers=headers)
-json_data = GenesisParam.json()
+headers          = {'content-type': 'application/json', 'project_id': BlockFrostId}
+GenesisParam     = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/genesis", headers=headers)
+json_data        = GenesisParam.json()
 
-epochLength = GenesisParam.json().get("epoch_length")
-activeSlotCoeff = GenesisParam.json().get("active_slots_coefficient")
-slotLength = GenesisParam.json().get("slot_length")
+epochLength      = GenesisParam.json().get("epoch_length")
+activeSlotCoeff  = GenesisParam.json().get("active_slots_coefficient")
+slotLength       = GenesisParam.json().get("slot_length")
 
 ### Epoch211FirstSlot ###
 firstShelleySlot = requests.get("https://cardano-mainnet.blockfrost.io/api/v0/blocks/4555184", headers=headers)
-json_data = firstShelleySlot.json()
-firstSlot = firstShelleySlot.json().get("slot")
+json_data        = firstShelleySlot.json()
+firstSlot        = firstShelleySlot.json().get("slot")
 
 ### calculate first slot of target epoch ###
 firstSlotOfEpoch = (firstSlot) + (epoch - 211)*epochLength
 
+### Math ###
 from decimal import *
 getcontext().prec = 9
 getcontext().rounding = ROUND_HALF_UP
@@ -220,7 +336,7 @@ for slot in range(firstSlotOfEpoch,epochLength+firstSlotOfEpoch):
         print("Epoch: " + str(epoch) + " - Local Time: " + str(timestamp.strftime('%Y-%m-%d %H:%M:%S') + " - Slot: " + str(slot-firstSlotOfEpoch)))
 
 print()
-print("Total Scheduled Blocks: " + str(slotcount))
+print("Total Leader Logs: " + str(slotcount))
 
 
 ### Epoch Assigned Performance or Luck
@@ -240,10 +356,12 @@ EpochLuck = int(100 * slotcount) / (blocksEpoch * pStake / nStake)
 
 print()
 print(f'Assigned Epoch Performance: ' + str(format(EpochLuck, ".2f")) + ' %' )
-
+print()
 
 
 if slotcount == 0:
     print()
-    print("No SlotLeader Schedules Found for Epoch: " +str(epoch))
+    print("No Leader Logs Found for Epoch: " +str(epoch))
     exit
+
+print()
